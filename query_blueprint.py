d04a1e163b1fa7ad3231890390d6eaaa78df4aaf
Zhihao Lyu <zhly4336@colorado.edu>
2024-12-13 17:59:46 -0700

    update query parameters

--- query_blueprint.py
+++ query_blueprint.py
@@ -14,11 +14,13 @@ query_blueprint = Blueprint('query_api', __name__, url_prefix='/query')
 async def most_recent_trips():
     prev = request.args.get('prev', default = 10, type = int)
     future = request.args.get('future', default = 5, type = int)
+    timepointid = request.args.get('timepointid', default = None, type = str)
+    runid = request.args.get('runid', default = None, type = str)
 
     current_time = datetime.now(timezone)
     midnight = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
     time_diff = current_time - midnight
     curr_time = int(time_diff.total_seconds())
 
-    res = await get_or_cache(curr_time=curr_time, prev=prev, future=future)
+    res = await get_or_cache(curr_time=curr_time, prev=prev, future=future, timepointid=timepointid, runid=runid)
     return res, 200
--- query_service.py
+++ query_service.py
@@ -12,13 +12,14 @@ redis_key_retention_time = 60
 
 
 async def get_or_cache(**kwargs):
-    sorted_keys = sorted(kwargs.keys())
+    cleaned_dict = {key: value for key, value in kwargs.items() if value is not None}
+    sorted_keys = sorted(cleaned_dict.keys())
     cache_key = "_".join([f"{key}{kwargs[key]}" for key in sorted_keys if key != "curr_time"])
     cache_res = await get_redis_client().get(cache_key)
     # if there is no cache, query the result and save it into Redis
     if cache_res is None:
         async for db_session in get_db():
-            query_res = await get_recent_trips(db_session, **kwargs)
+            query_res = await get_recent_trips(db_session, cleaned_dict)
             list_res = [obj.as_dict() for obj in query_res]
             grouped_dict = defaultdict(list)
             for item in list_res:
@@ -38,14 +39,23 @@ async def get_or_cache(**kwargs):
         return json_obj
 
 
-async def get_recent_trips(session, curr_time, prev, future):
-    prev_sec = prev * 60
-    future_sec = future * 60
+async def get_recent_trips(session, parm_dict):
+    curr_time = parm_dict["curr_time"]
+    prev_sec = parm_dict["prev"] * 60
+    future_sec = parm_dict["future"] * 60
     query = select(CsvData).filter(
         and_(CsvData.schd_time >= curr_time - prev_sec,
              CsvData.schd_time < curr_time + future_sec
         )
     )
+
+    # dynamic query
+    if "timepointid" in parm_dict:
+        query = query.filter(CsvData.timepointid == parm_dict["timepointid"])
+    
+    if "runid" in parm_dict:
+        query = query.filter(CsvData.runid == parm_dict["runid"])
+
     result = await session.execute(query)
     results = result.scalars().all()
     return results
